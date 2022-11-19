75e7eb7599e0cad9048d9d2d601bca81c4838c7b
2harden <zc_shenchn@outlook.com>
2022-11-19 10:23:24 +0800

    cmake summery

---
+++ Base64/getContent.py
@@ -0,0 +1,16 @@
+# import urllib
+
+# url="https://github.com/paimonhub/Paimonnode/blob/main/base64" # 这里是需要获取的网页
+# content=urllib.open(url).read() # 使用urllib模块获取网页内容
+# print (content) # 输出网页的内容，功能相当于查看网页源代码
+
+# import requests
+
+# url="https://raw.githubusercontent.com/paimonhub/Paimonnode/main/base64"
+# url="https://zhuanlan.zhihu.com/p/83535678"
+# s = requests.session()
+# s.keep_alive = False
+
+# content=requests.get(url).content
+# print (content)
+
---
+++ cmake/build/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/HelloWorld.cpp.obj"
+  "CMakeFiles/HelloWorld.dir/HelloWorld.cpp.obj.d"
+  "HelloWorld.exe"
+  "HelloWorld.exe.manifest"
+  "HelloWorld.pdb"
+  "libHelloWorld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
